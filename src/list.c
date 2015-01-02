cae63565ec0c78127610a0832f4d46951f11de0f
rzeszut <rzeszutekm@gmail.com>
2015-01-02 10:49:52 +0100

    Fixed memory leak in list.c

--- src/list.c
+++ src/list.c
@@ -14,18 +14,24 @@ struct list_t *list_create(void *data, struct list_t *next)
 
 void list_free(struct list_t *lst)
 {
-    if (!lst) return;
+    while (lst) {
+        struct list_t *next = lst->next;
 
-    list_free(lst->next);
-    free(lst);
+        free(lst);
+
+        lst = next;
+    }
 }
 
 void list_free_with_data(struct list_t *lst, void (*data_free)(void *))
 {
-    if (!lst) return;
+    while (lst) {
+        struct list_t *next = lst->next;
 
-    list_free(lst->next);
-    data_free(lst->data);
-    free(lst);
+        data_free(lst->data);
+        free(lst);
+
+        lst = next;
+    }
 }
 
